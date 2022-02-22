dd6f959574dc7847ed9f24a30378fa3ad3aba42d
jyostnaborse <jyostnaborse@gmail.com>
2022-02-22 00:45:21 +0000

    End of Section 3

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -15,22 +16,36 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductController(StoreContext context)
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
-       public async Task<ActionResult<List<Products>>> GetProducts(){
-           var products = await _context.Products.ToListAsync();
-           return Ok(products);
-       }
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _repo.GetProductAsync();
+            return Ok(products);
+        }
 
-       [HttpGet("{id}")]
-       public async Task<ActionResult<Products>> GetProduct(int id)
-       {
-           return await _context.Products.FindAsync(id);
-       }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _repo.GetProductByIdAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrand()
+        {
+            return Ok(await _repo.GetProductBrandAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductType()
+        {
+            return Ok(await _repo.GetProductTypeAsync());
+        }
+
     }
-} 
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
+using Core.Interfaces;
+using System.Linq;
 
 namespace API
 {
@@ -24,12 +26,16 @@ namespace API
         {
 
             services.AddControllers();
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddDbContext<StoreContext>(x => 
                     x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+                //c.ResolveConflictingActions (apiDescriptions => apiDescriptions.First());
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
